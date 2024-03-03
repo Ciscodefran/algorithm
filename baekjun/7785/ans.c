88be914c31547985889ab1ec52a545e68803e7b5
Ciscodefran <jellycisko@gmail.com>
2024-03-03 11:59:55 +0900

    7785
    bst gets time out

--- baekjun/7785/ans.c
+++ baekjun/7785/ans.c
@@ -8,60 +8,64 @@ typedef struct _NODE {
   struct _NODE *Left;
   struct _NODE *Right;
 } NODE;
-void makeNode(NODE *p, char *name, char status) {
-  p = (NODE *)malloc(sizeof(NODE));
-  strcpy(p->name, name);
-  p->status = status;
-  p->Left = NULL;
-  p->Right = NULL;
+
+void makeNode(NODE **p, char *name, char status) {
+  *p = (NODE *)malloc(sizeof(NODE));
+  strcpy((*p)->name, name);
+  (*p)->status = status;
+  (*p)->Left = NULL;
+  (*p)->Right = NULL;
 }
+
 // insert
-void insert(NODE *root, char *name, char status) {
-  if (root == NULL) {
+void insert(NODE **root, char *name, char status) {
+  if (*root == NULL) {
     makeNode(root, name, status);
   } else {
-    NODE *cur = root;
+    NODE *cur = *root;
     while (1) {
       int cmp = strcmp(cur->name, name);
       if (cmp > 0) {
         if (cur->Left == NULL) {
-          makeNode(cur->Left, name, status);
+          makeNode(&(cur->Left), name, status);
           break;
         } else {
           cur = cur->Left;
         }
       } else if (cmp < 0) {
         if (cur->Right == NULL) {
-          makeNode(cur->Right, name, status);
+          makeNode(&(cur->Right), name, status);
           break;
         } else {
           cur = cur->Right;
         }
       } else {
         cur->status = status;
+        break;
       }
     }
   }
 }
+
 void recursive_traverse(NODE *ptr) {
   if (ptr != NULL) {
-    recursive_traverse(ptr->Left);
-    printf("%s\n", ptr->name);
     recursive_traverse(ptr->Right);
+    if (ptr->status == 'e') printf("%s\n", ptr->name);
+    recursive_traverse(ptr->Left);
   }
 }
 
 int main(void) {
-  NODE *root;
+  NODE *root = NULL; // Initialize root to NULL
   int n;
   char name[6], status[6];
   scanf("%d", &n);
   for (int i = 0; i < n; i++) {
     scanf("%s %s", name, status);
     if (strcmp(status, "enter") == 0) {
-      insert(root, name, 'e');
+      insert(&root, name, 'e'); // Pass the address of root
     } else {
-      insert(root, name, 'l');
+      insert(&root, name, 'l');
     }
   }
   recursive_traverse(root);
