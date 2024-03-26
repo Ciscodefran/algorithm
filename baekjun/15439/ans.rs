c13a8b39fe14c23d7f01e4646630f3ad81ed7d95
Ciscodefran <jellycisko@gmail.com>
2024-03-26 22:28:05 +0700

    15439

---
+++ baekjun/15439/ans.rs
@@ -0,0 +1,8 @@
+use std::io;
+
+fn main() {
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).unwrap();
+    let n: u32 = buf.trim().parse().unwrap();
+    println!("{}", n * (n - 1));
+}
---
+++ baekjun/2346/ans.rs
@@ -0,0 +1,75 @@
+use std::io::{self, BufWriter, Write};
+
+struct Node {
+    number: u32,
+    value: i32,
+    front: *mut Node,
+    back: *mut Node,
+}
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut iter = buf.split_whitespace().map(|x| x.parse::<i32>().unwrap());
+    let n = iter.next().unwrap();
+    let head: *mut Node = &mut Node {
+        number: 1,
+        value: iter.next().unwrap(),
+        front: std::ptr::null_mut(),
+        back: std::ptr::null_mut(),
+    };
+    let mut prev: *mut Node = head;
+    for i in 0..n {
+        if i == 0 {
+            unsafe {
+                (*head).value = iter.next().unwrap();
+                continue;
+            }
+        } else if i == n - 1 {
+            unsafe {
+                (*prev).back = head;
+                (*head).front = prev;
+                break;
+            }
+        }
+        let node: *mut Node = &mut Node {
+            number: (i + 1) as u32,
+            value: iter.next().unwrap(),
+            front: prev,
+            back: std::ptr::null_mut(),
+        };
+        unsafe {
+            (*prev).back = node;
+        }
+        prev = node;
+    }
+
+    let mut cur: *mut Node = head;
+    let mut out = BufWriter::new(io::stdout());
+    for _ in 0..n {
+        unsafe {
+            let value = (*cur).value;
+            let number = (*cur).number;
+
+            write!(out, "{} ", number).unwrap();
+            writeln!(out, "{} ", value).unwrap();
+            // write!(out, "{} ", number).unwrap();
+            // pop_node(cur);
+            // for _ in 0..value.abs() {
+            //     if value > 0 {
+            //         cur = (*cur).front;
+            //     } else {
+            cur = (*cur).back;
+            //     }
+            // }
+        }
+    }
+}
+
+fn pop_node(node: *mut Node) {
+    unsafe {
+        let front = (*node).front;
+        let back = (*node).back;
+        (*front).back = back;
+        (*back).front = front;
+    }
+}
