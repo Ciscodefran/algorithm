1e99eba5df0bcb31ca0bf6d87cdcc521f885dd6d
Ciscodefran <jellycisko@gmail.com>
2024-03-29 07:02:06 +0700

    27433

---
+++ baekjun/27433/ans.rs
@@ -0,0 +1,15 @@
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let n: u32 = input.trim().parse().unwrap();
+    println!("{}", factorial(n));
+}
+
+fn factorial(n: u32) -> u32 {
+    if n == 0 {
+        return 1;
+    }
+    n * factorial(n - 1)
+}
