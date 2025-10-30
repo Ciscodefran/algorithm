c2c56649d8f4204ded8c1a3edfa536c17a454ca4
CharlieCisko <charlie@gdaytour.net>
2025-10-30 17:37:03 +0900

    1541

---
+++ baekjun/1541/ans.cpp
@@ -0,0 +1,35 @@
+#include <cctype>
+#include <iostream>
+using namespace std;
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  string str;
+  // 55-50+40
+  cin >> str;
+  int n = 0, sum = 0;
+  bool flag = true;
+  for (char c : str) {
+    if (!isdigit(c)) {
+      if (flag) {
+        sum += n;
+      } else {
+        sum -= n;
+      }
+      if (c == '-') {
+        flag = !flag;
+      }
+      n = 0;
+    } else {
+      n *= 10;
+      n += (c - '0');
+    }
+  }
+  if (flag) {
+    sum += n;
+  } else {
+    sum -= n;
+  }
+  cout << sum << endl;
+  return 0;
+}
