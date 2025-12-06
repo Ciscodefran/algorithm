6d526cb97aec9bd926e5dd4fd94b75faa86b9b69
Ciscodefran <jellycisko@gmail.com>
2025-12-06 12:05:22 +0900

    1520

--- baekjun/1520/ans.cpp
+++ baekjun/1520/ans.cpp
@@ -9,34 +9,12 @@ int main() {
   int n, m;
   cin >> n >> m;
   vector<vector<int>> v(n, vector<int>(m, 0));
+  vector<vector<int>> dp(n, vector<int>(m, 0));
   for (vector<int>& i : v) {
     for (int& j : i) {
       cin >> j;
     }
   }
-  queue<pair<int, int>> q;
-  q.push({0, 0});
-  int cnt = 0;
-  while (!q.empty()) {
-    auto [x, y] = q.front();
-    q.pop();
-    if (x == m - 1 && y == n - 1) {
-      ++cnt;
-      continue;
-    }
-    if (0 <= x && x < m - 1 && v[y][x + 1] < v[y][x]) {
-      q.push({x + 1, y});
-    }
-    if (0 < x && x < m && v[y][x - 1] < v[y][x]) {
-      q.push({x - 1, y});
-    }
-    if (0 <= y && y < n - 1 && v[y + 1][x] < v[y][x]) {
-      q.push({x, y + 1});
-    }
-    if (0 < y && y < n && v[y - 1][x] < v[y][x]) {
-      q.push({x, y - 1});
-    }
-  }
-  cout << cnt << endl;
+  dp[n - 1][m - 1] = 1;
   return 0;
 }
